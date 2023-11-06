768b95bc98cb7a40696cbe52bdb9d9878271553c
Ian Lehmann <ianlehman11@hotmail.com>
2023-11-06 19:05:49 -0300

    rondas ya resueltas

--- src/controlador/ApuestaController.java
+++ src/controlador/ApuestaController.java
@@ -8,19 +8,25 @@ import java.util.List;
 public class ApuestaController {
 
 	public int calcularPuntaje(List<Pronostico> listaPronosticos, Ronda ronda) {
+		
 		int puntajeTotal = 0;
+		
 		for(int i = 0; ronda.getPartidos().size()>=i; i++) {
+			
 				if(ronda.getPartidos().get(i).getEquipo1().getNombre() == listaPronosticos.get(i).getEquipo().getNombre()) {
 					listaPronosticos.get(i).setPartido(ronda.getPartidos().get(i));
 					puntajeTotal += listaPronosticos.get(i).puntos();
 				}
+				
 				else if(ronda.getPartidos().get(i).getEquipo2().getNombre() == listaPronosticos.get(i).getEquipo().getNombre()) {
 					listaPronosticos.get(i).setPartido(ronda.getPartidos().get(i));
 					puntajeTotal += listaPronosticos.get(i).puntos();
 					
 				}
+				
 				i++;
 		}
+		
 		return puntajeTotal;
 	}
 	
--- src/inicio/Main.java
+++ src/inicio/Main.java
@@ -15,39 +15,22 @@ public class Main {
 	public static void main(String[] args) {
 		
 	    PrincipalPI conector = new PrincipalPI();
+	    ApuestaController controlador = new ApuestaController();
 	    List<String[]> valores = conector.conectar();
+	    List<Partido> listaPartidos = new ArrayList<>();
 	    
-	    for (String[] line : valores) {
-	        String valor1 = line[0];
-	        String valor2 = line[1];
-	        String valor3 = line[2];
-	        String valor4 = line[3];
-
-	        System.out.println("Valor 1: " + valor1);
-	        System.out.println("Valor 2: " + valor2);
-	        System.out.println("Valor 3: " + valor3);
-	        System.out.println("Valor 3: " + valor4);
+	    for(String[] line : valores) {
+	        Partido partido = new Partido(
+	        		new Equipo(line[0]),
+	        		new Equipo(line[3]),
+	        		Integer.parseInt(line[1]),
+	        		Integer.parseInt(line[2])
+	        		);
+	        listaPartidos.add(partido);
 	    }
-	}
-		
-//    	ApuestaController controlador = new ApuestaController();
-//		Equipo equipo1 = new Equipo("Boca", "Equipo Argentino");
-//		Equipo equipo2 = new Equipo("River", "Equipo Argentino");
-//		Equipo equipo3 = new Equipo("Barcelona", "Equipo Español");
-//		Equipo equipo4 = new Equipo("Real Madrid", "Equipo Español");
-//		Equipo equipo5 = new Equipo("Racing", "Equipo Argentino");
-//		Equipo equipo6 = new Equipo("Independiente", "Equipo Argnentino");
-//		
-//		Partido partido1 = new Partido(equipo1, equipo2, 2, 1);
-//		Partido partido2 = new Partido(equipo3, equipo4, 0, 0);
-//		Partido partido3 = new Partido(equipo5, equipo6, 1, 0);
-//		
-//		List<Partido> listaPartidos = new ArrayList<>();
-//		listaPartidos.add(partido1);
-//		listaPartidos.add(partido2);
-//		listaPartidos.add(partido3);
-//		
-//		Ronda ronda = new Ronda("1", listaPartidos);
+	
+
+	Ronda ronda = new Ronda("1", listaPartidos);
 //		
 //		Pronostico pronostico1 = new Pronostico(equipo1, ResultadoEnum.ganador);
 //		Pronostico pronostico2 = new Pronostico(equipo1, ResultadoEnum.ganador);
@@ -57,6 +40,6 @@ public class Main {
 //		listaPronosticos.add(pronostico1);
 //		listaPronosticos.add(pronostico2);
 //		listaPronosticos.add(pronostico3);
-//		
-//		System.out.println(controlador.calcularPuntaje(listaPronosticos, ronda));
-	}
+		
+		System.out.println(controlador.calcularPuntaje(listaPronosticos, ronda));
+}
